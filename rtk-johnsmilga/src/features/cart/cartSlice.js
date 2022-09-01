b4ea76b86dfbdd83de678b529fe64872f3b540e1
scoobytux <tu.lna07@gmail.com>
2022-09-01 19:43:10 +0700

    learn: 8 - remove item

--- rtk-johnsmilga/src/features/cart/cartSlice.js
+++ rtk-johnsmilga/src/features/cart/cartSlice.js
@@ -15,9 +15,15 @@ const cartSlice = createSlice({
     clearCart: state => {
       state.cartItems = [];
     },
+    removeItem: (state, { payload }) => {
+      const itemId = payload;
+      state.cartItems = state.cartItems.filter(
+        cartItem => cartItem.id !== itemId
+      );
+    },
   },
 });
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
