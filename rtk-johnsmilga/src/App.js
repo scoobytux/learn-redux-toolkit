f487ac8defacab3c7eb09549224fb47b8c20968c
scoobytux <tu.lna07@gmail.com>
2022-09-02 16:26:34 +0700

    learn: 11 - create modal

--- rtk-johnsmilga/src/App.js
+++ rtk-johnsmilga/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
 
 function App() {
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
